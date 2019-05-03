cfdd4e1fa8d7dc6d22101303fdc4258463a3d557
hopper723 <hopper723@gmail.com>
2019-05-02 17:56:23 -0700

    first commit

---
+++ AR Ruler/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  AR Ruler
+//
+//  Created by Hiu Man Yeung on 5/2/19.
+//  Copyright © 2019 Hiu Man Yeung. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+}
